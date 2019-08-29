f9829df1de85a3994978337fbaba163758ef470d
Lukman055 <lkmnsuryaa@gmail.com>
2019-08-29 16:48:07 +0700

    belajar array

---
+++ fungsi07.php
@@ -0,0 +1,7 @@
+<?php
+if (function_exists('exif_read_data')) {
+    echo "funsi exif_read_data() ada di PHP.<br />\n>";    
+}else {
+    echo "fungsi exif_read_data() tidak ada di PHP.<br />\n>";
+}
+?>
